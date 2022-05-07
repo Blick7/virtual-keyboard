05c4524b61550a70f9f4def1cbaeb2654507ecd3
Blick <45530302+Blick7@users.noreply.github.com>
2022-05-07 12:42:09 +0300

    init: start virtual-keyboard

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import './styles/main.scss';
+
+// const keysEn = [
+//   '`',
+//   '1',
+//   '2',
+//   '3',
+//   '4',
+//   '5',
+//   '6',
+//   '7',
+//   '8',
+//   '9',
+//   '0',
+//   '-',
+//   '=',
+//   'Backspace',
+//   '',
+// ];
+
+document.addEventListener('keydown', (event) => {
+  // eslint-disable-next-line no-console
+  console.log(event);
+  // dasd
+});
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+let mode = 'development';
+let target = 'web'; // dont used browserslist in dev mode
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
+  target = 'browserslist'; // use browserslist in production
+}
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: './src/index.html',
+  }),
+  new MiniCssExtractPlugin({
+    filename: '[name].[contenthash].css',
+  }),
+  new ESLintPlugin(),
+];
+
+module.exports = {
+  mode,
+  target,
+  plugins,
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/[hash][ext][query]',
+    clean: true,
+  },
+  devtool: 'source-map',
+  devServer: {
+    hot: true,
+    static: {
+      //! important to fix hot reload
+      directory: path.join(__dirname, 'src'),
+      watch: true,
+    },
+  },
+  module: {
+    rules: [
+      { test: /\.(html)$/, use: ['html-loader'] },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+        type: mode === 'production' ? 'asset' : 'asset/resource',
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true, // Use cache to prevent recompilation on every run
+          },
+        },
+      },
+    ],
+  },
+};
