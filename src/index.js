0c23af898a5387e4bc3a5119685b5290b3635049
Blick <45530302+Blick7@users.noreply.github.com>
2022-05-08 11:22:04 +0300

    feat: add layout and basic keyboard functionality

--- src/index.js
+++ src/index.js
@@ -1,25 +1,53 @@
+// import styles
 import './styles/main.scss';
 
-// const keysEn = [
-//   '`',
-//   '1',
-//   '2',
-//   '3',
-//   '4',
-//   '5',
-//   '6',
-//   '7',
-//   '8',
-//   '9',
-//   '0',
-//   '-',
-//   '=',
-//   'Backspace',
-//   '',
-// ];
+// import js
+import { keys } from './js/keys';
+import { keyboard } from './js/keyboard';
+
+document.body.innerHTML = keyboard;
+const rows = document.querySelectorAll('.row');
+const textarea = document.querySelector('.text');
+
+const generateKeyboard = () => {
+  // eslint-disable-next-line no-console
+  console.log(keys.enKeys);
+  rows.forEach((item, index) => {
+    for (let i = 0; i < item.children.length; i += 1) {
+      const elem = item.children[i];
+      elem.innerHTML = keys.enKeys[index][i];
+      if (elem.innerHTML === 'Space') {
+        elem.innerHTML = ' ';
+      }
+    }
+  });
+};
+
+document.addEventListener('DOMContentLoaded', generateKeyboard);
+textarea.innerHTML += 'D';
 
 document.addEventListener('keydown', (event) => {
-  // eslint-disable-next-line no-console
-  console.log(event);
-  // dasd
+  event.preventDefault(); // remove default action from space, tab and etc
+  const buttons = document.querySelectorAll('[data-code]');
+  const { code } = event;
+  buttons.forEach((button) => {
+    if (button.dataset.code === code) {
+      // eslint-disable-next-line no-console
+      console.log(button);
+      textarea.innerHTML += button.textContent;
+      button.classList.add('active');
+    }
+  });
+});
+
+document.addEventListener('keyup', (event) => {
+  const buttons = document.querySelectorAll('[data-code]');
+  const { code } = event;
+  buttons.forEach((button) => {
+    if (button.dataset.code === code) {
+      // eslint-disable-next-line no-console
+      console.log(button);
+      button.classList.remove('active');
+    }
+  });
 });
---
+++ src/js/keyboard.js
@@ -0,0 +1,81 @@
+export const keyboard = `
+<div class="container">
+        <textarea class="text" autofocus></textarea>
+        <div class="keyboard">
+            <div class="row">
+                <div class="button" data-code="Backquote"></div>
+                <div class="button" data-code="Digit1"></div>
+                <div class="button" data-code="Digit2"></div>
+                <div class="button" data-code="Digit3"></div>
+                <div class="button" data-code="Digit4"></div>
+                <div class="button" data-code="Digit5"></div>
+                <div class="button" data-code="Digit6"></div>
+                <div class="button" data-code="Digit7"></div>
+                <div class="button" data-code="Digit8"></div>
+                <div class="button" data-code="Digit9"></div>
+                <div class="button" data-code="Digit0"></div>
+                <div class="button" data-code="Minus"></div>
+                <div class="button" data-code="Equal"></div>
+                <div class="button" data-code="Backspace"></div>
+            </div>
+            <div class="row">
+                <div class="button" data-code="Tab"></div>
+                <div class="button" data-code="KeyQ"></div>
+                <div class="button" data-code="KeyW"></div>
+                <div class="button" data-code="KeyE"></div>
+                <div class="button" data-code="KeyR"></div>
+                <div class="button" data-code="KeyT"></div>
+                <div class="button" data-code="KeyY"></div>
+                <div class="button" data-code="KeyU"></div>
+                <div class="button" data-code="KeyI"></div>
+                <div class="button" data-code="KeyO"></div>
+                <div class="button" data-code="KeyP"></div>
+                <div class="button" data-code="BracketLeft"></div>
+                <div class="button" data-code="BracketRight"></div>
+                <div class="button" data-code="Backslash"></div>
+                <div class="button" data-code="Delete"></div>
+            </div>
+            <div class="row">
+                <div class="button" data-code="CapsLock"></div>
+                <div class="button" data-code="KeyA"></div>
+                <div class="button" data-code="KeyS"></div>
+                <div class="button" data-code="KeyD"></div>
+                <div class="button" data-code="KeyF"></div>
+                <div class="button" data-code="KeyG"></div>
+                <div class="button" data-code="KeyH"></div>
+                <div class="button" data-code="KeyJ"></div>
+                <div class="button" data-code="KeyK"></div>
+                <div class="button" data-code="KeyL"></div>
+                <div class="button" data-code="Semicolon"></div>
+                <div class="button" data-code="Quote"></div>
+                <div class="button" data-code="Enter"></div>
+            </div>
+            <div class="row">
+                <div class="button" data-code="ShiftLeft"></div>
+                <div class="button" data-code="KeyZ"></div>
+                <div class="button" data-code="KeyX"></div>
+                <div class="button" data-code="KeyC"></div>
+                <div class="button" data-code="KeyV"></div>
+                <div class="button" data-code="KeyB"></div>
+                <div class="button" data-code="KeyN"></div>
+                <div class="button" data-code="KeyM"></div>
+                <div class="button" data-code="Comma"></div>
+                <div class="button" data-code="Period"></div>
+                <div class="button" data-code="Slash"></div>
+                <div class="button" data-code="ArrowUp"></div>
+                <div class="button" data-code="ShiftRight"></div>
+            </div>
+            <div class="row">
+                <div class="button" data-code="ControlLeft"></div>
+                <div class="button" data-code="Lang"></div>
+                <div class="button" data-code="AltLeft"></div>
+                <div class="button" data-code="Space"></div>
+                <div class="button" data-code="AltRight"></div>
+                <div class="button" data-code="ArrowLeft"></div>
+                <div class="button" data-code="ArrowDown"></div>
+                <div class="button" data-code="ArrowRight"></div>
+                <div class="button" data-code="ControlRight"></div>
+            </div>
+        </div>
+    </div>
+`;
